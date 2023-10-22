5a584a2a20a9e09d87097ecdaf3e50f03d7c340b
Kraken57 <kasaad1609@gmail.com>
2023-10-23 00:36:12 +0530

    files in lua

--- 10files.lua
+++ 10files.lua
@@ -5,7 +5,7 @@
 io.write("Hello World!")
 ]]
 
-
+--[[
 --now to input from the file 
 io.input("myfile.txt")
 
@@ -14,3 +14,20 @@ local fileData = io.read("*all")--we have to read everything from the file
 print(fileData)
 
 io.close()
+]]
+
+
+--another way of working with files 
+local file = io.open("myfile.txt", "w")
+--for reading the file
+local file = io.open("myfile.txt", "r")
+--to append use "a" like local file = io.open("myfile.txt", "a")
+
+if file ~= nil then
+    file:write("Hello there!")
+    print(file:read("*all"))--reading the file
+    --to read line by line we use real("*line")
+    file:close() 
+else
+    print("Could not open the file")
+end
