2a5a526e1f5aa4e289d5f4884412d3cfa1aef77f
Kraken57 <kasaad1609@gmail.com>
2023-10-23 00:13:48 +0530

    functions in lua

--- 09function.lua
+++ 09function.lua
@@ -16,3 +16,25 @@ end
 myName("Ahmad")
 myName("Saad")
 myName()
+
+
+local function sum(num1, num2)
+    local val = num1+num2
+    --but you can write return in conditions
+    if val == 10 then
+        return val 
+    end 
+
+    if val < 100 then 
+        return -val
+    end
+    
+    return val * 2
+    --after return you must not write anything because nothing will execute after return
+
+end
+
+local x = 2
+local ans = sum(1,x)
+
+print("The answer is: " .. ans)
